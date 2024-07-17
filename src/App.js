eddcaa2743693eb356eea9269993b1d8811208bb
Nishant Nagar <nishantnagar54321@gmail.com>
2024-07-17 15:42:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [colors, setColors] = useState(Array(9).fill("white"));
+  const [clickedOrder, setClickedOrder] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = (index) => {
+    let newColors = [...colors];
+    let newClickedOrder = [...clickedOrder];
+
+    if (!newClickedOrder.includes(index)) {
+      newClickedOrder.push(index);
+      newColors[index] = "green";
+      setClickedOrder(newClickedOrder);
+      setColors(newColors);
+    }
+
+    setCounter(counter + 1);
+
+    if (counter === 8) {
+      let delay = 0;
+      newClickedOrder.forEach((clickedIndex) => {
+        setTimeout(() => {
+          newColors[clickedIndex] = "orange";
+          setColors([...newColors]);
+        }, delay);
+        delay += 500;
+      });
+    }
+  };
+
+  const handleReset = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="matrix">
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className="box"
+            style={{ backgroundColor: color }}
+            onClick={() => handleClick(index)}
+          ></div>
+        ))}
+      </div>
+      <button className="reset-button" onClick={handleReset}>
+        Reset
+      </button>
     </div>
   );
-}
+};
 
 export default App;
